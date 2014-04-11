c64ecfcf17cebd0ddca1693557f01c54544b70db
Brad W <bwilso65@uwo.ca>
2014-04-11 11:24:10 -0400

    Fixed overwrite and add refactored code

---
+++ password-keeper/password-keeper/Utility/Utilities.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password_keeper.Utility
+{
+    static class Utilities
+    {
+        //return true if site exists, false otherwise
+        public static bool entryExists(String site, String fileName)
+        {
+            try
+            {
+                //assuming the file isn't going to be very big, so I can read it all into memory
+                string[] content = File.ReadAllLines(fileName);
+                foreach (String line in content)
+                {
+                    if (line.Contains(site)) return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+                return false;
+            }
+            return false;
+        }
+
+        //creates a new entry containing a site name and an associated password
+        public static bool entryInsert(String site, String fileName)
+        {
+            try
+            {
+                StreamWriter sw = File.AppendText(fileName);
+                sw.WriteLine("\n" + site + " : " + generatePassword() + "\n");
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        //Overwrites an existing password with a new one
+        public static bool entryOverwrite(String site, String fileName)
+        {
+            try
+            {
+                //List<String> lines = File.ReadLines(fileName).ToList();
+                List<String> lines = File.ReadAllLines(fileName).ToList<String>();
+                String newLine = null;
+                foreach (String line in lines)
+                {
+                    if (line.Contains(site))
+                    {
+                        newLine = "\n" + site + " : " + generatePassword() + "\n";
+                        lines.Remove(line);
+                        lines.Add(newLine);
+                        break;
+                    }
+                }
+                File.WriteAllLines(fileName, lines);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        //method to generate a random password
+        private static String generatePassword()
+        {
+            string strPwdchar = "abcdefghijklmnopqrstuvwxyz0123456789#+@&$ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string strPwd = "";
+
+            // Use a 4-byte array to fill it with random bytes and convert it then
+            // to an integer value.
+            byte[] randomBytes = new byte[4];
+
+            // Generate 4 random bytes.
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(randomBytes);
+
+            // Convert 4 bytes into a 32-bit integer value.
+            int seed = (randomBytes[0] & 0x7f) << 24 |
+                        randomBytes[1] << 16 |
+                        randomBytes[2] << 8 |
+                        randomBytes[3];
+
+            // Now, this is real randomization.
+            Random rnd = new Random(seed);
+
+            //generate the password
+            for (int i = 0; i <= rnd.Next(12, 18); i++)
+            {
+                int iRandom = rnd.Next(0, strPwdchar.Length - 1);
+                strPwd += strPwdchar.Substring(iRandom, 1);
+            }
+            return strPwd;
+
+        }
+    }
+}
--- password-keeper/password-keeper/password-keeper.cs
+++ password-keeper/password-keeper/password-keeper.cs
@@ -43,6 +43,7 @@ namespace password_keeper
             //parse the command line
             Arguments CommandLine = new Arguments(args);
             //checking if file name is provided
+            #region "F"
             if (CommandLine["f"] != null)
             {
                 fileName = CommandLine["f"];
@@ -59,7 +60,9 @@ namespace password_keeper
                 Console.WriteLine("Please enter it using -f {fileName}");
                 return;
             }
+            #endregion
             //check if site name is present
+            #region "S"
             if (CommandLine["s"] != null)
             {
                 siteName = CommandLine["s"];
@@ -70,15 +73,15 @@ namespace password_keeper
                 Console.WriteLine("Please enter it using -s {site-name}");
                 return;
             }
-
+            #endregion
             //check if printing or putting to clipboard
             if (CommandLine["p"] != null) print = true;
-
+            #region "a"
             //make a new entry
             if (CommandLine["a"] != null)
             {
                 String c = null;
-                if (entryExists(siteName, fileName))
+                if (Utilities.entryExists(siteName, fileName))
                 {
                     //ask if user wants to replace
                     Console.WriteLine("A site with that name already exists.");
@@ -92,7 +95,7 @@ namespace password_keeper
                     if (c == "y")
                     {
                         //overwrite the appropriate line
-                        if (entryOverwrite(siteName, fileName))
+                        if (Utilities.entryOverwrite(siteName, fileName))
                         {
                             Console.WriteLine(siteName + " added to list.");
                         }
@@ -106,7 +109,7 @@ namespace password_keeper
                 else
                 {
                     //add new entry
-                    if (entryInsert(siteName, fileName))
+                    if (Utilities.entryInsert(siteName, fileName))
                     {
                         Console.WriteLine(siteName + " added to list.");
                     }
@@ -117,6 +120,7 @@ namespace password_keeper
                     }
                 }
             }
+            #endregion
 
             //done parsing command line...
             //time to retrieve the login information and display it or copy to clipboard
@@ -144,103 +148,6 @@ namespace password_keeper
                 Clipboard.SetText(pswd);
                 Console.WriteLine("Password saved onto clipboard");
             }
-        }
-
-        //return true if site exists, false otherwise
-        private static bool entryExists(String site, String fileName)
-        {
-            try
-            {
-                //assuming the file isn't going to be very big, so I can read it all into memory
-                string[] content = File.ReadAllLines(fileName);
-                foreach (String line in content)
-                {
-                    if (line.Contains(site)) return true;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.Write(ex.ToString());
-                return false;
-            }
-            return false;
-        }
-
-        //creates a new entry containing a site name and an associated password
-        private static bool entryInsert(String site, String fileName)
-        {
-            try
-            {
-                StreamWriter sw = File.AppendText(fileName);
-                sw.WriteLine("\n" + site + " : " + generatePassword());
-                sw.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-            return true;
-        }
-
-        //Overwrites an existing password with a new one
-        private static bool entryOverwrite(String site, String fileName)
-        {
-            try
-            {
-                List<String> lines = File.ReadLines(fileName).ToList<String>();
-                String newLine = null;
-                foreach (String line in lines)
-                {
-                    if (line.Contains(site))
-                    {
-                        newLine = "\n" + site + " : " + generatePassword();
-                        lines.Remove(line);
-                        lines.Add(newLine);
-                    }
-                }
-                File.WriteAllLines(fileName, lines);
-            
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
-            return true;
-        }
-
-        //method to generate a random password
-        private static String generatePassword()
-        {
-            string strPwdchar = "abcdefghijklmnopqrstuvwxyz0123456789#+@&$ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string strPwd = "";
-
-            // Use a 4-byte array to fill it with random bytes and convert it then
-            // to an integer value.
-            byte[] randomBytes = new byte[4];
-
-            // Generate 4 random bytes.
-            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-            rng.GetBytes(randomBytes);
-
-            // Convert 4 bytes into a 32-bit integer value.
-            int seed = (randomBytes[0] & 0x7f) << 24 |
-                        randomBytes[1] << 16 |
-                        randomBytes[2] << 8 |
-                        randomBytes[3];
-
-            // Now, this is real randomization.
-            Random rnd = new Random(seed);
-            
-            //generate the password
-            for (int i = 0; i <= rnd.Next(12,18); i++)
-            {
-                int iRandom = rnd.Next(0, strPwdchar.Length - 1);
-                strPwd += strPwdchar.Substring(iRandom, 1);
-            }
-            return strPwd;
-            
         }
     }
 }
